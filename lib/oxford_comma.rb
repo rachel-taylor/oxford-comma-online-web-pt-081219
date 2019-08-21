cd3883cddc7b3c59033802e5b5c67098907dc0a1
Rachel T <rachmaybe@gmail.com>
2019-08-21 01:40:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   s = ""
   i = 0 
   if array.length == 1 
-    return array [0]
+    return array[0]
   elseif array.length == 2 
     return "#{array[0]} and #{array[1]}"
   else 
@@ -10,7 +10,7 @@ def oxford_comma(array)
     s << "#{array[i]},"
     i += 1 
   end 
-  s << "and #{array[array.length-1]}"
+  s << "and #{array[array.length -1]}"
   return s
 end 
 end
