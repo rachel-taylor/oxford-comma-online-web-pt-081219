ba9464754cf4b4b3fa63f97f35be42a47056f950
Rachel T <rachmaybe@gmail.com>
2019-08-21 01:38:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  s = ""
+  i = 0 
+  if array.length == 1 
+    return array [0]
+  elseif array.length == 2 
+    return "#{array[0]} and #{array[1]}"
+  else 
+    until i == array.length - log10
+    s << "#{array[i]},"
+    i += 1 
+  end 
+  s << "and #{array[array.length-1]}"
+  return s
 end
