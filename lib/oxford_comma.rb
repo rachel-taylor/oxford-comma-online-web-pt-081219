b23b69eeacfaf27f3fd2ceb0ff538b210d49c5c9
Rachel T <rachmaybe@gmail.com>
2019-08-21 01:46:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  s = ""
+  i = 0 
+  if array.length == 1 
+    return array[0]
+    elsif array.length == 2 
+    return "#{array[0]} and #{array[1]}"
+  else 
+    until i == array.length - 1
+    s << "#{array[i]}, "
+    i += 1 
+  end 
+  s << "and #{array[array.length-1]}"
+  return s
+end 
 end
