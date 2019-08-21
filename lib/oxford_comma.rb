f33341824f13f8eaa6082dc715b9c1d3ca0216f2
Rachel T <rachmaybe@gmail.com>
2019-08-21 01:45:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   i = 0 
   if array.length == 1 
     return array[0]
-  elseif array.length == 2 
+    elseif array.length == 2 
     return "#{array[0]} and #{array[1]}"
   else 
     until i == array.length - 1
